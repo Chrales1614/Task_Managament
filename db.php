090b5a28cdd46cfa6d9c5102a86e325d49332874
Chrales1614 <delgadocharles1614@gmail.com>
2025-02-12 10:48:06 +0800

    new db

--- db.php
+++ db.php
@@ -1,27 +1,14 @@
 <?php
-include('db.php');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "task_management";
 
-$id = $_GET['id'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $due_date = $_POST['due_date'];
-    $priority = $_POST['priority'];
-    $status = $_POST['status'];
-
-    $sql = "UPDATE tasks SET title='$title', description='$description', due_date='$due_date', 
-            priority='$priority', status='$status' WHERE id=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        header("Location: index.php");
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-// Fetch the current task details
-$sql = "SELECT * FROM tasks WHERE id=$id";
-$result = $conn->query($sql);
-$task = $result->fetch_assoc();
 ?>
